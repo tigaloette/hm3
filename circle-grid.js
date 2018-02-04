2d4564c92318abb4c9245e2486db7309d7d1e9e9
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-02-04 13:36:39 -0800

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,7 @@
+background (0);
+ for (var x = 15; x < width; x += (width-15)/20) {
+   for (var y = 15; y < height; y += (height-15)/20){
+     fill(255,255,255);
+     ellipse (x, y, 10, 10);
+     }
+   }
