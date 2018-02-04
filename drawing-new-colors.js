0ce8eb4299d80c4b0370144eab738336b11c81e5
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-02-04 15:51:41 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,27 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(225, 70, 70);
+  } else if (key == 'G') {
+    fill(150, 225, 110);
+  } else if (key == 'B') {
+    fill(50, 200, 215); 
+  } else if (key == "Y") {
+		fill(255, 211, 0)
+	}	else if (key == "P") {
+		fill(192, 5, 230)
+	}
+
+}
